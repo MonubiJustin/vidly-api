b3345108c86e02c7a426c33c2d6043b814ebdc8a
MonubiJustin <monubijustin@gmail.com>
2025-05-22 00:47:11 +0300

    added connection string

--- startup/db.js
+++ startup/db.js
@@ -3,9 +3,10 @@ const logger = require('../logger/logger')
 const config = require('config')
 
 module.exports = function () {
-    const db = config.get('db')
-    mongoose.connect(db, {
+    // const db = config.get('db')
+    
+    mongoose.connect('mongodb+srv://michelle:zxcqwe123%40@cluster0.eo6fuyv.mongodb.net/myDatabase?retryWrites=true&w=majority&appName=Cluster0', {
         serverSelectionTimeoutMS: 5000
     })
-        .then(() => logger.info(`Connected to ${db}...`))
+        .then(() => logger.info(`Connected to db...`))
 }
